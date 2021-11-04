9bb41d6ea1b331e795f9e5d7f5c02531d6a4a5e3
Muhammad Al-amin <muhammad.alamindev01@gmail.com>
2021-11-04 20:27:28 +0600

    single linked list

---
+++ single_item_linked_list.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+typedef struct node Node;
+struct node
+{
+    int data;
+    Node *next;
+};
+
+Node *create_node(int item, Node *next);
+
+int main()
+{
+    Node *n;
+    n = create_node(2247, NULL);
+    cout << "Data -> " << n->data << endl;
+
+    return 0;
+}
+
+// create linked list
+Node *create_node(int item, Node *next)
+{
+    Node *new_node = new Node;
+    if(new_node == NULL)
+    {
+        cout << "Error! Could not Create A new Node.\n";
+        exit(1);
+    }
+
+    new_node->data = item;
+    new_node->next = next;
+
+    return new_node;
+}
